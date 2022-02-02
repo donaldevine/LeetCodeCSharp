93d8d3308a9b89c6b46743781f0653e3be76b3bd
Donal Devine <5518040+donaldevine@users.noreply.github.com>
2022-02-02 23:32:35 +0000

    added solution

---
+++ LeetCodeCSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TwoSumSolution twoSumSolution = new TwoSumSolution();
+
+            int[] nums = { 2, 7, 11, 15 };
+
+            var twoSumResult = twoSumSolution.TwoSum(nums, 9);
+
+            Console.WriteLine("[{0}]", string.Join(", ", twoSumResult));
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LeetCodeCSharp/TwoSumSolution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp
+{
+    public class TwoSumSolution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+
+            int arrayLength = nums.Length;
+
+            Dictionary<int, int> resultDictionary = new Dictionary<int, int>();
+
+            if (nums == null || arrayLength < 2)
+            {
+                return new int[] { };
+            }
+
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                int firstNumber = nums[i];
+                int secondNumber = target - firstNumber;
+
+                if (resultDictionary.TryGetValue(secondNumber, out int index))
+                {
+                    return new[] { index, i };
+                }
+
+                resultDictionary[firstNumber] = i;
+            }
+
+            return new int[] { };
+        }
+    }
+}
